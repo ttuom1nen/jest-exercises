974888c7be034d0ebf5910efd48cbe483f0c4566
ttuom1nen <ttuominen@outlook.com>
2020-11-26 22:53:19 +0200

    Add mock

--- script2.test.js
+++ script2.test.js
@@ -4,9 +4,26 @@ const swapi = require("./script2");
 it("calls swapi to get people", () => {
   // expect.assertions() good to use with async tests
   // expect is an assertion
-  expect.assertions(2)
-  return swapi.getPeopleAsync(fetch).then(data => {
-    expect(data.count).toEqual(82)
-    expect(data.results.length).toBeGreaterThan(5)
-  })
-})
+  expect.assertions(2);
+  return swapi.getPeopleAsync(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
+
+// This is a 'mock' 
+it("getPeople returns count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () => Promise.resolve({ count: 82, results: [0, 1, 2, 3, 4, 5] }),
+    })
+  );
+
+  expect.assertions(4);
+  return swapi.getPeopleAsync(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.dev/api/people");
+    expect(data.count).toEqual(82);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
